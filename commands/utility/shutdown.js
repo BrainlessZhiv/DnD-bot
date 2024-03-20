a09ece1554be7ed50cee05b762ee478792d7e65e
Cueto Fernández <68489984+BrainlessZhiv@users.noreply.github.com>
2024-03-20 19:52:18 +0100

    Refactor shutdown command to include permission check

--- commands/utility/shutdown.js
+++ commands/utility/shutdown.js
@@ -1,12 +1,24 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('shutdown')
-        .setDescription('Shuts the bot down.'),
-    async execute(interaction) {
-        await interaction.reply({content: 'Shutting down...', ephemeral: true});
-        console.log('<@' + interaction.user.id + '> is shutting me down from slash command.');
-        process.exit();
-    },
+  data: new SlashCommandBuilder()
+    .setName("shutdown")
+    .setDescription("Shuts the bot down."),
+  async execute(interaction) {
+    console.log(
+      "<@" +
+        interaction.user.id +
+        "> is trying to shut me down from slash command."
+    );
+    // Check if the user has the 'ADMINISTRATOR' permission
+    if (!interaction.member.permissions.has("ADMINISTRATOR")) {
+      await interaction.reply({
+        content: "You do not have permission to use this command.",
+        ephemeral: true,
+      });
+      return;
+    }
+      await interaction.reply({ content: 'Shutting down...', ephemeral: true });
+      process.exit();
+    }
 };
