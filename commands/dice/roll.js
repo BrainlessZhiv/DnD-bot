188e31de16f888f15a5ba9fd0d7e9c74169da4c1
Cueto Fernández <68489984+BrainlessZhiv@users.noreply.github.com>
2024-03-20 19:24:54 +0100

    Add options for target number and lower check in roll command

--- commands/dice/roll.js
+++ commands/dice/roll.js
@@ -5,16 +5,68 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("roll")
     .setDescription("Rolls a dice")
-    .addStringOption(option =>
+    .addStringOption((option) =>
       option
         .setName("formula")
         .setDescription("The dice formula to roll")
         .setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("target")
+        .setDescription("The target number to compare the roll to")
+        .setRequired(false)
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName("lower")
+        .setDescription(
+          "Whether the dice check is for lower rather than higher than the target number"
+        )
+        .setRequired(false)
     ),
   async execute(interaction) {
-    calculateDice(interaction.options.getString("formula"));
-    
-    await interaction.reply({ content: interaction.options.getString("formula") + " = " + calculateDice(interaction.options.getString("formula")), ephemeral: false });
-    
-},
+    const reply = await interaction.deferReply();
+
+    const calculation = calculateDice(interaction.options.getString("formula"));
+    diceResults = calculation.diceResults;
+    modResults = calculation.modResults;
+    result = calculation.result;
+
+    diceResults = diceResults.join(" + ");
+    modResults = modResults.join("");
+
+    if (interaction.options.getInteger("target")) {
+      if (interaction.options.getBoolean("lower")) {
+        if (result <= interaction.options.getInteger("target")) {
+          checkResult = "Success";
+        } else {
+          checkResult = "Failure";
+        }
+      } else {
+        if (result >= interaction.options.getInteger("target")) {
+          checkResult = "Success";
+        } else {
+          checkResult = "Failure";
+        }
+      }
+    }
+
+    await interaction.editReply({
+      content:
+        "``" +
+        interaction.options.getString("formula") +
+        "`` = ``" +
+        diceResults +
+        "" +
+        modResults +
+        " = " +
+        result +
+        "``" +
+        (interaction.options.getInteger("target")
+          ? " Target: ``" + interaction.options.getInteger("target") + "`` Result: ``" + checkResult + "``"
+          : ""),
+      ephemeral: false,
+    });
+  },
 };
