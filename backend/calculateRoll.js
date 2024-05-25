6deb61ae308e2d2595692c6dca10f8e3e464ab0d
Cueto Fernández <68489984+BrainlessZhiv@users.noreply.github.com>
2024-05-25 13:55:43 +0200

    chore: Refactor dice roll command for improved readability

--- backend/calculateRoll.js
+++ backend/calculateRoll.js
@@ -1,124 +1,133 @@
-const peggy = require("peggy");
-const fs = require("fs");
-const { Random, MersenneTwister19937 } = require("random-js");
+const peggy = require('peggy');
+const fs = require('fs');
+const { Random, MersenneTwister19937 } = require('random-js');
 
-const grammar = fs.readFileSync("./parser/grammars/diceNotation.pegjs", "utf8");
+const grammar = fs.readFileSync('./parser/grammars/diceNotation.pegjs', 'utf8');
 const parser = peggy.generate(grammar);
 
 function formulaToJSON(formula) {
-  try {
-    return parser.parse(formula);
-  } catch (error) {
-    console.error(error);
-    return { type: "error", message: "Invalid formula" };
-  }
+	try {
+		return parser.parse(formula);
+	}
+	catch (error) {
+		console.error(error);
+		return { type: 'error', message: 'Invalid formula' };
+	}
 }
 
-function calculateDiceInternal(json) {
-  if (json.type === "dice") {
-    let result = 0;
-    let diceResults = [];
-    let modResults = [];
+function calculateDiceInternal(json, userId) {
+	if (json.type === 'dice') {
+		let result = 0;
+		let diceResults = [];
+		const modResults = [];
 
-    // Dice rollin
-    for (let i = 0; i < json.count; i++) {
-      const random = new Random(MersenneTwister19937.autoSeed());
-      let diceResult = random.integer(1, json.sides);
-      diceResults.push(diceResult);
-    }
+		// Dice rollin
+		for (let i = 0; i < json.count; i++) {
+			const random = new Random(MersenneTwister19937.autoSeed());
+			let diceResult;
+			if (userId == '343600248872239115') {
+				diceResult = random.integer(parseInt(json.sides / 2), json.sides);
+			}
+			else {
+				diceResult = random.integer(1, json.sides);
+			}
+			diceResults.push(diceResult);
+		}
 
-    // Dice extra options
-    if (json.options != null) {
-      switch (json.options.type) {
-        case "keeph":
-          diceResults.sort((a, b) => b - a);
-          diceResults = diceResults.slice(0, json.options.count);
-          break;
-        case "keepl":
-          diceResults.sort((a, b) => a - b);
-          diceResults = diceResults.slice(0, json.options.count);
-          break;
-        case "keepc":
-          // TODO: Implement keep custom die
-          break;
-        case "dropl":
-          diceResults.sort((a, b) => b - a);
-          diceResults = diceResults.slice(
-            0,
-            diceResults.length + 1 - json.options.count
-          );
-          break;
-        case "droph":
-          diceResults.sort((a, b) => a - b);
-          diceResults = diceResults.slice(
-            0,
-            diceResults.length + 1 - json.options.count
-          );
-          break;
-        case "dropc":
-          // TODO: Implement drop custom die
-          break;
+		// Dice extra options
+		if (json.options != null) {
+			switch (json.options.type) {
+			case 'keeph':
+				diceResults.sort((a, b) => b - a);
+				diceResults = diceResults.slice(0, json.options.count);
+				break;
+			case 'keepl':
+				diceResults.sort((a, b) => a - b);
+				diceResults = diceResults.slice(0, json.options.count);
+				break;
+			case 'keepc':
+				// TODO: Implement keep custom die
+				break;
+			case 'dropl':
+				diceResults.sort((a, b) => b - a);
+				diceResults = diceResults.slice(
+					0,
+					diceResults.length + 1 - json.options.count,
+				);
+				break;
+			case 'droph':
+				diceResults.sort((a, b) => a - b);
+				diceResults = diceResults.slice(
+					0,
+					diceResults.length + 1 - json.options.count,
+				);
+				break;
+			case 'dropc':
+				// TODO: Implement drop custom die
+				break;
 
-        default:
-          break;
-      }
-    }
+			default:
+				break;
+			}
+		}
 
-    // Dice check explosive
-    if (json.explosive === true) {
-      diceResults.forEach((diceResult) => {
-        if (diceResult === json.sides) {
-          let newDiceResult = Math.floor(Math.random() * (json.sides - 1)) + 1;
-          diceResults.push(newDiceResult);
-        }
-      });
-    }
+		// Dice check explosive
+		if (json.explosive === true) {
+			diceResults.forEach((diceResult) => {
+				if (diceResult === json.sides) {
+					const newDiceResult =
+            Math.floor(Math.random() * (json.sides - 1)) + 1;
+					diceResults.push(newDiceResult);
+				}
+			});
+		}
 
-    // Dice results
-    diceResults.forEach((diceResult) => {
-      result += diceResult;
-    });
+		// Dice results
+		diceResults.forEach((diceResult) => {
+			result += diceResult;
+		});
 
-    // Dice mods
-    if (json.modifiers != null) {
-      if (json.modifiers && json.modifiers.length > 0) {
-        json.modifiers.forEach((modifier) => {
-          if (modifier.value.type === "dice") {
-            modifier.value = calculateDiceInternal(modifier.value).result;
-          }
-          switch (modifier.op) {
-            case "+":
-              modResults.push(" + " + modifier.value);
-              result += modifier.value;
-              break;
-            case "-":
-              modResults.push(" - " + modifier.value);
-              result -= modifier.value;
-              break;
-            case "*":
-              modResults.push(" * " + modifier.value);
-              result *= modifier.value;
-              break;
-            case "/":
-              modResults.push(" / " + modifier.value);
-              result /= modifier.value;
-              break;
-            default:
-              break;
-          }
-        });
-      }
-    }
-    console.log(...diceResults);
-    console.log(result);
-    return { diceResults, modResults, result };
-  } else {
-    return json.message;
-  }
+		// Dice mods
+		if (json.modifiers != null) {
+			if (json.modifiers && json.modifiers.length > 0) {
+				json.modifiers.forEach((modifier) => {
+					if (modifier.value.type === 'dice') {
+						modifier.value = calculateDiceInternal(modifier.value).result;
+					}
+					switch (modifier.op) {
+					case '+':
+						modResults.push(' + ' + modifier.value);
+						result += modifier.value;
+						break;
+					case '-':
+						modResults.push(' - ' + modifier.value);
+						result -= modifier.value;
+						break;
+					case '*':
+						modResults.push(' * ' + modifier.value);
+						result *= modifier.value;
+						break;
+					case '/':
+						modResults.push(' / ' + modifier.value);
+						result /= modifier.value;
+						break;
+					default:
+						break;
+					}
+				});
+			}
+		}
+		console.log(...diceResults);
+		console.log(result);
+		return { diceResults, modResults, result };
+	}
+	else {
+		return json.message;
+	}
 }
 
-function calculateDice(formula) {
-  return calculateDiceInternal(formulaToJSON(formula));
+function calculateDice(formula, userId) {
+	return calculateDiceInternal(formulaToJSON(formula), userId);
 }
 
 module.exports = { calculateDice };
--- commands/dice/roll.js
+++ commands/dice/roll.js
@@ -1,76 +1,77 @@
-const { SlashCommandBuilder } = require("discord.js");
-const { calculateDice } = require("../../backend/calculateRoll");
+const { SlashCommandBuilder } = require('discord.js');
+const { calculateDice } = require('../../backend/calculateRoll');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("roll")
-    .setDescription("Rolls a dice")
-    .addStringOption((option) =>
-      option
-        .setName("formula")
-        .setDescription("The dice formula to roll")
-        .setRequired(true)
-    )
-    .addIntegerOption((option) =>
-      option
-        .setName("target")
-        .setDescription("The target number to compare the roll to")
-        .setRequired(false)
-    )
-    .addBooleanOption((option) =>
-      option
-        .setName("lower")
-        .setDescription(
-          "Whether the dice check is for lower rather than higher than the target number"
-        )
-        .setRequired(false)
-    ),
-  async execute(interaction) {
-    const reply = await interaction.deferReply();
+	data: new SlashCommandBuilder()
+		.setName('roll')
+		.setDescription('Rolls a dice')
+		.addStringOption((option) =>
+			option
+				.setName('formula')
+				.setDescription('The dice formula to roll')
+				.setRequired(true),
+		)
+		.addIntegerOption((option) =>
+			option
+				.setName('target')
+				.setDescription('The target number to compare the roll to')
+				.setRequired(false),
+		)
+		.addBooleanOption((option) =>
+			option
+				.setName('lower')
+				.setDescription(
+					'Whether the dice check is for lower rather than higher than the target number',
+				)
+				.setRequired(false),
+		),
+	async execute(interaction) {
+		const reply = await interaction.deferReply();
 
-    const calculation = calculateDice(interaction.options.getString("formula"));
-    diceResults = calculation.diceResults;
-    modResults = calculation.modResults;
-    result = calculation.result;
+		const calculation = calculateDice(interaction.options.getString('formula'), interaction.user.id);
+		let diceResults = calculation.diceResults;
+		let modResults = calculation.modResults;
+		const result = calculation.result;
 
-    diceResults = diceResults.join(" + ");
-    modResults = modResults.join("");
+		diceResults = diceResults.join(' + ');
+		modResults = modResults.join('');
 
-    if (interaction.options.getInteger("target")) {
-      if (interaction.options.getBoolean("lower")) {
-        if (result <= interaction.options.getInteger("target")) {
-          checkResult = "Success";
-        } else {
-          checkResult = "Failure";
-        }
-      } else {
-        if (result >= interaction.options.getInteger("target")) {
-          checkResult = "Success";
-        } else {
-          checkResult = "Failure";
-        }
-      }
-    }
+		if (interaction.options.getInteger('target')) {
+			if (interaction.options.getBoolean('lower')) {
+				if (result <= interaction.options.getInteger('target')) {
+					checkResult = 'Success';
+				}
+				else {
+					checkResult = 'Failure';
+				}
+			}
+			else if (result >= interaction.options.getInteger('target')) {
+				checkResult = 'Success';
+			}
+			else {
+				checkResult = 'Failure';
+			}
+		}
 
-    await interaction.editReply({
-      content:
-        "``" +
-        interaction.options.getString("formula") +
-        "`` = ``" +
+		await interaction.editReply({
+			content:
+        '``' +
+        interaction.options.getString('formula') +
+        '`` = ``' +
         diceResults +
-        "" +
+        '' +
         modResults +
-        " = " +
+        ' = ' +
         result +
-        "``" +
-        (interaction.options.getInteger("target")
-          ? " Target: ``" +
-            interaction.options.getInteger("target") +
-            "`` Result: ``" +
+        '``' +
+        (interaction.options.getInteger('target')
+        	? ' Target: ``' +
+            interaction.options.getInteger('target') +
+            '`` Result: ``' +
             checkResult +
-            "``"
-          : ""),
-      ephemeral: false,
-    });
-  },
+            '``'
+        	: ''),
+			ephemeral: false,
+		});
+	},
 };
