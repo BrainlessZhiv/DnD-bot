a5d2b172d79aebf00285a361404c2c5629b926a9
Cueto Fernández <68489984+BrainlessZhiv@users.noreply.github.com>
2024-05-25 19:05:49 +0200

    chore: Refactor deploy-commands.js for improved readability and maintainability

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,19 +12,19 @@ const commands = [];
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 for (const folder of commandFolders) {
-    	// Grab all the command files from the commands directory you created earlier
-    const commandFiles = fs.readdirSync(`${foldersPath}/${folder}`).filter(file => file.endsWith('.js'));
-    	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-        for (const file of commandFiles) {
-        const command = require(`${foldersPath}/${folder}/${file}`);
-        if ('data' in command) {
-            commands.push(command.data.toJSON());
-            console.log(`[INFO]: Command ${file} at ${foldersPath}/${folder} has been loaded.`);
+	// Grab all the command files from the commands directory you created earlier
+	const commandFiles = fs.readdirSync(`${foldersPath}/${folder}`).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const command = require(`${foldersPath}/${folder}/${file}`);
+		if ('data' in command) {
+			commands.push(command.data.toJSON());
+			console.log(`[INFO]: Command ${file} at ${foldersPath}/${folder} has been loaded.`);
 		}
 		else {
-            console.error(`[WARNING]: Command ${file} at ${foldersPath}/${folder} is not a valid command file.`);
-        }
-    }
+			console.error(`[WARNING]: Command ${file} at ${foldersPath}/${folder} is not a valid command file.`);
+		}
+	}
 }
 
 // Construct and prepare an instance of the REST module
